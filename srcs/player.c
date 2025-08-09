5e9830eea6470323d255b27f6fa09b05e3f31926
a-hakki <abdelazizhatake@gmail.com>
2025-08-09 19:41:29 +0100

    player organized 1%

--- srcs/player.c
+++ srcs/player.c
@@ -6,7 +6,7 @@
 /*   By: ahakki <ahakki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 23:19:02 by ahakki            #+#    #+#             */
-/*   Updated: 2025/08/09 09:26:34 by ahakki           ###   ########.fr       */
+/*   Updated: 2025/08/09 19:40:45 by ahakki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,70 +29,82 @@ void	init_player(t_game *game)
 	player->z_eye = 0.5;
 }
 
-
-
-int	move_player(t_game *game)	
+int is_blocked(float x, float y, t_game *game)
 {
-	t_player	*player = &game->player;
-	float angle_speed = 0.05;
-	float z_eye_speed = 0.05;
-
-	// ROTATION left right
-	if (player->left_rotate)
-		player->angle -= angle_speed;
-	if (player->right_rotate)
-		player->angle += angle_speed;
-	// ROTATION up down
-	if (player->up_rotate && player->z_eye < 0.7)
-		player->z_eye += z_eye_speed;
-	if (player->down_rotate && player->z_eye > 0.3)
-		player->z_eye -= z_eye_speed;
-
-	// Normalize angle (keep between 0 and 2*PI)
-	if (player->angle > 2 * PI)
-		player->angle -= 2 * PI;
-	if (player->angle < 0)
-		player->angle += 2 * PI;
-
-	// Compute keyection
-	float new_x = 0;
-	float new_y = 0;
-
-	// FORWARD
-	if (player->key_up)
-	{
-		new_x = player->x + cos(player->angle) * player->speed;
-		new_y = player->y + sin(player->angle) * player->speed;
-	}
-	if (player->key_down)
-	{
-		new_x = player->x + cos(player->angle + PI) * player->speed;
-		new_y = player->y + sin(player->angle + PI) * player->speed;
-	}
-	if (player->key_right)
-	{
-		new_x = player->x + cos(player->angle + PI / 2) * player->speed;
-		new_y = player->y + sin(player->angle + PI / 2) * player->speed;
-	}
-	if (player->key_left)
-	{
-		new_x = player->x + cos(player->angle - PI / 2) * player->speed;
-		new_y = player->y + sin(player->angle - PI / 2) * player->speed;
-	}
-	if (!touch(new_x, player->y, game) && !touch(player->x, new_y, game))
-	{
-		player->x = new_x;
-		player->y = new_y;
-	}
-	else
-	{
-		if (!touch(new_x, player->y, game))
-			player->x += (new_x - player->x) * 0.2;
-		if (!touch(player->x, new_y, game))
-			player->y += (new_y - player->y) * 0.2;
-	}
-
-	return (0);
+    return touch(x - BLOCK / 4, y - BLOCK / 4, game) ||
+           touch(x + BLOCK / 4, y - BLOCK / 4, game) ||
+           touch(x - BLOCK / 4, y + BLOCK / 4, game) ||
+           touch(x + BLOCK / 4, y + BLOCK / 4, game);
 }
 
+static void	normalize_angle(t_player *player)
+{
+    if (player->angle > 2 * PI)
+        player->angle -= 2 * PI;
+    if (player->angle < 0)
+        player->angle += 2 * PI;
+}
 
+static void	apply_rotation(t_player *player)
+{
+    float angle_speed = 0.05;
+    float z_eye_speed = 0.05;
+
+    if (player->left_rotate)
+        player->angle -= angle_speed;
+    if (player->right_rotate)
+        player->angle += angle_speed;
+    if (player->up_rotate && player->z_eye < 0.7)
+        player->z_eye += z_eye_speed;
+    if (player->down_rotate && player->z_eye > 0.3)
+        player->z_eye -= z_eye_speed;
+    normalize_angle(player);
+}
+
+static void	calc_move(t_player *player, float *new_x, float *new_y)
+{
+    if (player->key_up)
+    {
+        *new_x = player->x + cos(player->angle) * player->speed;
+        *new_y = player->y + sin(player->angle) * player->speed;
+    }
+    else if (player->key_down)
+    {
+        *new_x = player->x + cos(player->angle + PI) * player->speed;
+        *new_y = player->y + sin(player->angle + PI) * player->speed;
+    }
+    else if (player->key_right)
+    {
+        *new_x = player->x + cos(player->angle + PI / 2) * player->speed;
+        *new_y = player->y + sin(player->angle + PI / 2) * player->speed;
+    }
+    else if (player->key_left)
+    {
+        *new_x = player->x + cos(player->angle - PI / 2) * player->speed;
+        *new_y = player->y + sin(player->angle - PI / 2) * player->speed;
+    }
+}
+
+int	move_player(t_game *game)
+{
+    t_player	*player = &game->player;
+    float		new_x = player->x;
+    float		new_y = player->y;
+
+    apply_rotation(player);
+    calc_move(player, &new_x, &new_y);
+
+    if (!is_blocked(new_x, player->y, game) && !is_blocked(player->x, new_y, game))
+    {
+        player->x = new_x;
+        player->y = new_y;
+    }
+    else
+    {
+        if (!is_blocked(new_x, player->y, game))
+            player->x += (new_x - player->x) * 0.2;
+        if (!is_blocked(player->x, new_y, game))
+            player->y += (new_y - player->y) * 0.2;
+    }
+    return (0);
+}
