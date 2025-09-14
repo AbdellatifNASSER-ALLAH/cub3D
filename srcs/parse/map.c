0d85d383a709c492b95ab528a206ab5c8141f154
Abdellatif NASSER-ALLAH <nasserallahabdellatif2@gmail.com>
2025-09-14 18:47:28 +0100

    that was not good I need new algo for the validation of the map

--- srcs/parse/map.c
+++ srcs/parse/map.c
@@ -6,7 +6,7 @@
 /*   By: abdnasse <abdnasse@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/11 16:50:41 by abdnasse          #+#    #+#             */
-/*   Updated: 2025/09/10 09:32:53 by abdnasse         ###   ########.fr       */
+/*   Updated: 2025/09/14 18:45:10 by abdnasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,4 @@
 
 #include "cub3d.h"
 
-void	fill_map(char **map,int	start, int end, t_config *cfg)
-{
-	t_line	l;
-
-	(void)cfg;
-	while(start < end)
-	{
-		wrap_line(&l, map[start]);
-		start++;
-	}
-}
+void	fill_map(char **map,int	start, int end, t_config *cfg);
