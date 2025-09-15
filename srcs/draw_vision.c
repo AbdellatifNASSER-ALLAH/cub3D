aeeb06cd2364544c097ac3132e5e70a2fa72ab25
a-hakki <abdelazizhatake@gmail.com>
2025-09-15 11:39:24 +0100

    simplifying

--- srcs/draw_vision.c
+++ srcs/draw_vision.c
@@ -6,7 +6,7 @@
 /*   By: ahakki <ahakki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/14 09:01:41 by ahakki            #+#    #+#             */
-/*   Updated: 2025/09/14 09:42:13 by ahakki           ###   ########.fr       */
+/*   Updated: 2025/09/15 09:45:38 by ahakki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,19 +22,6 @@ void	calc_dist_and_height(t_ray *r, t_player *player);
 void	select_color(t_ray *r, t_game *game);
 void	draw_vision(t_game *game);
 
-// Handles drawing a vertical stripe
-void	draw_stripe(int x, t_ray *r, t_game *game)
-{
-	int	y;
-
-	y = 0;
-	while (y < r->start_y)
-		put_pixel(x, y++, 0x87CEEB, game);
-	while (y < r->end_y && y < HEIGHT)
-		put_pixel(x, y++, r->color, game);
-	while (y < HEIGHT)
-		put_pixel(x, y++, 0x654321, game);
-}
 
 void	init_ray_dir_and_map(t_ray *r, t_player *player, int x)
 {
@@ -132,6 +119,20 @@ void	calc_dist_and_height(t_ray *r, t_player *player)
 	r->wall_height = (BLOCK / r->dist) * (WIDTH / 2);
 	r->start_y = (HEIGHT - r->wall_height) * player->z_eye;
 	r->end_y = r->start_y + r->wall_height;
+}
+
+// Handles drawing a vertical stripe
+void	draw_stripe(int x, t_ray *r, t_game *game)
+{
+	int	y;
+
+	y = 0;
+	while (y < r->start_y)
+		put_pixel(x, y++, 0x87CEEB, game);
+	while (y < r->end_y && y < HEIGHT)
+		put_pixel(x, y++, r->color, game);
+	while (y < HEIGHT)
+		put_pixel(x, y++, 0x654321, game);
 }
 
 void	select_color(t_ray *r, t_game *game)
